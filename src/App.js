b69eaa84788b3d950b8775cd31b7c5790f5cbd9b
luiss1569 <rcsrcs1569@gmail.com>
2020-01-15 11:49:41 -0300

    pagina feita

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./styles/global.css"
+import "./styles/sidebar.css"
+import "./styles/main.css"
+import axios from 'axios'
 
 function App() {
+  const [latitude, setLatidude] = useState('')
+  const [longitude, setLongitude] = useState('')
+  const [github_username, setGitUsername] = useState('')
+  const [techs, setTechs] = useState('')
+  const [devs, setDevs] = useState([])
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+        setLatidude(latitude)
+        setLongitude(longitude)
+      },
+      (err) => { },
+      {
+        timeout: 30000,
+      }
+    )
+  }, [])
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    const response = await axios.post('http://localhost:3333/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    })
+
+    setGitUsername('')
+    setTechs('')
+    setDevs([...devs, response.data])
+  }
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await axios.get('http://localhost:3333/devs')
+      setDevs(response.data)
+    }
+    loadDevs()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input className="github_username" id="github_username" value={github_username} onChange={e => setGitUsername(e.target.value)} required></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input className="techs" id="techs" value={techs} onChange={e => setTechs(e.target.value)} required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input className="latitude" type="number" id="latitude" value={latitude} onChange={e => setLatidude(e.target.value)} required></input>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input className="longitude" type="number" id="longitude" value={longitude} onChange={e => setLongitude(e.target.value)} required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <li className="dev-item" key={dev._id}>
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="dev-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil do GitHub</a>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
